a057af5140e4ba1c22786159b4f0c02dc2b7e7c1
dake0505 <dakejiang123@163.com>
2022-05-18 22:23:25 +0800

    update ts

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,15 @@
 import babel from 'rollup-plugin-babel'
+import path from 'path'
+import ts from 'rollup-plugin-typescript'
+import { DEFAULT_EXTENSIONS } from '@babel/core'
+
+const getPath = _path => path.resolve(__dirname, _path)
+const extensions = ['.js', '.ts', '.tsx']
+
+const tsPlugin = ts({
+  tsconfig: getPath('./tsconfig.json'),
+  extensions
+})
 
 export default {
   input: './src/index.js',
@@ -6,6 +17,15 @@ export default {
     file: './lib/bundle.js',
     format: 'esm'
   },
-  plugins: [babel()],
+  plugins: [babel({
+    runtimeHelpers: true,
+    // 只转换源代码，不运行外部依赖
+    exclude: 'node_modules/**',
+    // babel 默认不支持 ts 需要手动添加
+    extensions: [
+      ...DEFAULT_EXTENSIONS,
+      '.ts',
+    ],
+  }), tsPlugin],
   external: ['react']
 }
