214653331b603769344c7aeafe7152d2ca802a54
dake0505 <dakejiang123@163.com>
2022-05-23 22:44:43 +0800

    feat(modal): init props

---
+++ src/Components/Modal/Modal.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export interface ModalProps {
+  visible?: boolean;
+  title?: React.ReactNode;
+  closable?: boolean;
+  onOk?: (e: React.MouseEvent<HTMLElement>) => void;
+  onCancel?: (e: React.MouseEvent<HTMLElement>) => void;
+}
